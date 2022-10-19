3596b33f44c9231042b188ef8a29f58753eb5843
Miguellouis123 <mmladiaz@tip.edu.ph>
2022-10-19 14:27:40 +0800

    Quizupdate

---
+++ src/views/Quiz.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+  <h1 style="color:#A52A2A;">About Composition API</h1>
+  <p style="color:black;">
+      
+      
+
+      The Composition API consists of a number of different Vue.js internals exposed via utility functions. 
+      It helps to better organize component code by logical concern and makes it easier to separate component logic from the component 
+      template and even reuse that logic across multiple components. Finally, it brings better Typescript support to Vue.js. </p>
+<hr>
+
+  </div>
+</template>
